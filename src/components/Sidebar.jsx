4ca58e270c09b4dd145c69a41a2546485bcab8e9
hiroaki <blackcoff0722@gmail.com>
2025-03-16 07:52:29 +0900

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+
+const Sidebar = () => {
+    return (
+        <div className="bg-gray-800">
+            
+            <h1> Dashboard </h1>
+
+            <ul>
+                <li>Home</li>
+                <li>Analytics</li>
+                <li>Setting</li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+import Sidebar from "../components/Sidebar";
+
+
+
+const Dashboard = () => {
+    return (
+        <div className="flex">
+
+            <Sidebar />
+            <div>
+                
+            </div>
+
+        </div>
+    );
+};
+
+
+export default Dashboard;
